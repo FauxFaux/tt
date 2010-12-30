e6f1b834752e14b9e46c90fe797de3db992e3554
Chris West (Faux) <git@goeswhere.com>
2010-12-30 15:36:02 +0000

    Mavenise

--- tt-backend/src/main/java/com/goeswhere/tt/TT.java
+++ tt-backend/src/main/java/com/goeswhere/tt/TT.java
@@ -1,3 +1,5 @@
+package com.goeswhere.tt;
+
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
--- tt-backend/src/main/java/com/goeswhere/tt/TT2.java
+++ tt-backend/src/main/java/com/goeswhere/tt/TT2.java
@@ -1,3 +1,5 @@
+package com.goeswhere.tt;
+
 import java.util.Arrays;
 
 
